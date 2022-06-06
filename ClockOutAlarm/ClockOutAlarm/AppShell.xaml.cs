66adf3863be8efa865ced2529d791a93d92b85ec
Jordan Neal <jordaneal@gmail.com>
2022-06-06 13:04:31 -0700

    Add project files.

---
+++ ClockOutAlarm/ClockOutAlarm/AppShell.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ClockOutAlarm.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ClockOutAlarm
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(HomePage), typeof(HomePage));
+            Routing.RegisterRoute(nameof(AlarmsPage), typeof(AlarmsPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ ClockOutAlarm/ClockOutAlarm/Views/HomePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ClockOutAlarm.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            Play();
+        }
+
+        public void Play()
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            Stream audioStream = assembly.GetManifestResourceStream("ClockOutAlarm.Resources.jobDone.mp3");
+            var audio = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+            audio.Load(audioStream);
+
+            audio.Play();
+        }
+    }
+}
